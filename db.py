811de02ac46467c4bea96fa2db2b6c9d444970a5
romildo <romildo-rrj@hotmail.com>
2025-05-10 10:38:46 -0300

    fix: Remove aspas duplicada

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ def conectar():
             host=os.getenv('DB_HOST'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
-            database="os.getenv('DB_NAME')"
+            database=os.getenv('DB_NAME')
         )
         return conexao
 
